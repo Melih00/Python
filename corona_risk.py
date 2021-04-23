15f9bd9964bdf1dd580eeaecf1cb7aee35a95bef
Melih00 <melihagilli0@gmail.com>
2021-04-23 13:41:02 +0300

    edited corona_risk.py

--- corona_risk.py
+++ corona_risk.py
@@ -1,7 +1,17 @@
 age = input('Are you a cigarette addict older than 75 years old? (answer as "Yes" or"No")').lower().strip()
 chronic = input('Do you have a severe chronic disease (answer as "Yes" or"No")').lower().strip()
 immune = input('Is your immune system too weak (answer as "Yes" or"No")').lower().strip()
-if age == 'yes' or chronic == 'yes' or immune == 'yes':
-    print('You are in risky group')
+mylist = []
+if age in ['yes', 'no']:
+    if chronic in ['yes', 'no']:
+        if immune in ['yes', 'no']:
+            if age == 'yes' or chronic == 'yes' or immune == 'yes':
+               print('You are in risky group')
+            else:
+                print('You are not in risky group')
+        else:
+            print('please enter as "Yes" or "No"')
+    else:
+            print('please enter as "Yes" or "No"')
 else:
-    print('You are not in risky group')
+            print('please enter as "Yes" or "No"')
