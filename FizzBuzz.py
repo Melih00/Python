ed7024c47d092d9bc1c2a2b882701babcbeaed31
Melih00 <melihagilli0@gmail.com>
2021-05-05 17:21:08 +0300

    FizzBuzz.py added

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+a = 'Fizz'
+b = 'Buzz'
+for i in range(1,101):
+  if i % 15 == 0:
+    print(f'{a}{b}')
+  elif i % 3 == 0:
+    print(f'{a}')
+  elif i % 5 == 0:
+    print(f'{b}')
+  else:
+    print(i)
